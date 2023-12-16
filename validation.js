d4bc86e42afe2210754edc0fc8b458ab8aadb2d3
Yasminedh22 <yasmine.dhaouadi@esprit.tn>
2023-12-16 19:32:26 +0100

    validation

--- validation.js
+++ validation.js
@@ -4,29 +4,25 @@ function validateForm() {
   var email = document.getElementById('email').value;
   var message = document.getElementById('message').value;
 
-  // Validate First Name (No numeric characters)
   if (!/^[a-zA-Z]+$/.test(firstName)) {
     alert('Please enter a valid first name without numeric characters.');
     return false;
   }
 
-  // Validate Last Name (No numeric characters)
   if (!/^[a-zA-Z]+$/.test(lastName)) {
     alert('Please enter a valid last name without numeric characters.');
     return false;
   }
 
-  // Validate Email
   if (!/^\S+@\S+\.\S+$/.test(email)) {
     alert('Please enter a valid email address.');
     return false;
   }
 
-  // Validate Message
   if (message.trim() === '') {
     alert('Please enter a message.');
     return false;
   }
 
-  return true; // Form is valid
+  return true; 
 }
